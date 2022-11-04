48cd19d1431569826fa6f4df0ae0c2126aaea2b8
vulcandth <vulcandth@gmail.com>
2022-11-03 19:32:34 -0500

    Follow Mons Port
    Leaf Follow Code
    
    Credit: AtmaBuster's Leaf Follow code from Crystal Leaf
    
    Add Additional Following Unown Sprites
    
    Credits to FrenchOrange and LJSTAR for sprites.
    
    Bugfix: Surfing directly across a map connection does not load the new map
    
    Bugfix from pret/pokecrystal docs/bugs_and_glitches.md
    
    Bugfix: ReadObjectEvents overflows into wObjectMasks
    
    Fix warp invisible and minor optimization
    
    More Follower Interactions
    
    AddPokeballSprite
    
    OW Pokeball close animation
    
    Follower Pokeball is Emote
    
    Speed up pokeball animation
    
    PokeBallEmote
    
    CherrygroveCity
    
    ElmsLab
    
    CheckActiveBall, NewballAnim, GetFirstAlive offbyonefix
    
    Shiny OW Mons & use party pals
    
    Fix follower event script nick.
    
    add `jump_in_place` movement (NPC only)
    
    Resolve X_FLIP issues in party screen.
    
    Additional map updates
    
    `Script_appearfollower` jumps to wrong bank (never working?)
    
    Finish map scripts
    
    Pokecenter & MrPokemon interaction
    
    Co-Authored-By: AtmaBuster <73505949+AtmaBuster@users.noreply.github.com>
    Co-Authored-By: FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>

--- engine/overworld/overworld.asm
+++ engine/overworld/overworld.asm
@@ -84,6 +84,7 @@ RefreshSprites::
 	xor a
 	ld [hUsedSpriteIndex], a
 	call ReloadSpriteIndex
+	call LoadMiscTiles
 	pop bc
 	pop de
 	pop hl
@@ -137,6 +138,9 @@ LoadMiscTiles:
 	bit 6, a
 	ret nz
 
+	ld c, EMOTE_POKE_BALL
+	farcall LoadEmote
+
 	ld c, EMOTE_SHADOW
 	farcall LoadEmote
 	call GetMapEnvironment
@@ -155,6 +159,8 @@ SafeGetSprite:
 	ret
 
 GetSprite::
+	call GetFollowingSprite
+	ret c
 	call GetMonSprite
 	ret c
 
@@ -192,39 +198,28 @@ GetMonSprite:
 	jr z, .BreedMon2
 	cp SPRITE_VARS
 	jr nc, .Variable
-	jr .Icon
+	jr .pokemon_sprite
 
 .Normal:
 	and a
 	ret
 
-.Icon:
+.pokemon_sprite:
 	sub SPRITE_POKEMON
 	ld e, a
 	ld d, 0
 	ld hl, SpriteMons
 	add hl, de
 	ld a, [hl]
-	jr .Mon
+	jp GetWalkingMonSprite
 
 .BreedMon1
 	ld a, [wBreedMon1Species]
-	jr .Mon
+	jp GetWalkingMonSprite
 
 .BreedMon2
 	ld a, [wBreedMon2Species]
-
-.Mon:
-	ld e, a
-	and a
-	jr z, .NoBreedmon
-
-	farcall LoadOverworldMonIcon
-
-	ld l, WALKING_SPRITE
-	ld h, 0
-	scf
-	ret
+	jp GetWalkingMonSprite
 
 .Variable:
 	sub SPRITE_VARS
@@ -236,16 +231,114 @@ GetMonSprite:
 	and a
 	jp nz, GetMonSprite
 
-.NoBreedmon:
-	ld a, WALKING_SPRITE
-	ld l, WALKING_SPRITE
+GetFirstAliveMon::
+; Returns [wParty#Sprite] in a; party number in d
+	ld a, [wPartyCount]
+	and a
+	ret z
+	inc a
+	ld d, 1
+	ld e, a
+	ld bc, wPartyMon1
+.loop
+	ld hl, MON_HP
+	add hl, bc
+	ld a, [hli]
+	push de
+	ld d, a
+	ld a, [hl]
+	or d
+	pop de
+	jr nz, .got_mon_struct
+	inc d
+	ld a, d
+	cp e
+	ret z
+	ld hl, PARTYMON_STRUCT_LENGTH
+	add hl, bc
+	ld b, h
+	ld c, l
+	jr .loop
+.got_mon_struct
+	ld a, [bc]
+	ret
+
+GetFollowingSprite:
+	cp SPRITE_FOLLOWER
+	jr nz, GetWalkingMonSprite.nope
+
+	call GetFirstAliveMon
+	ld [wFollowerSpriteID], a
+	push af
+	ld a, d
+	ld [wFollowerPartyNum], a
+	pop af
+
+GetWalkingMonSprite:
+	push af
+	dec a
+
+	ld hl, FollowingSpritePointers
+
+	cp (UNOWN - 1) ; we already decremented
+	jr nz, .not_unown
+	ld a, [wFollowerPartyNum]
+	ld bc, PARTYMON_STRUCT_LENGTH
+	ld hl, wPartyMon1DVs
+	call AddNTimes
+	predef GetUnownLetter
+	ld a, [wUnownLetter]
+	dec a
+	ld hl, UnownFollowingSpritePointers
+
+.not_unown
+	ld d, 0
+	ld e, a
+	add hl, de
+	add hl, de
+	add hl, de
+	assert BANK(FollowingSpritePointers) == BANK(UnownFollowingSpritePointers), \
+			"FollowingSpritePointers Bank is not equal to UnownFollowingSpritePointers"
+	ld a, BANK(FollowingSpritePointers)
+	push af
+	call GetFarByte
+	ld b, a
+	inc hl
+	pop af
+	call GetFarWord
+
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wDecompressScratch)
+	ldh [rSVBK], a
+
+	push bc
+	ld a, b
+	ld de, wDecompressScratch
+	call FarDecompress
+	pop bc
+	ld de, wDecompressScratch
+
+	pop af
+	ldh [rSVBK], a
+
 	ld h, 0
+	ld c, 12
+	ld l, WALKING_SPRITE
+
+	pop af
+
+	scf
+	ret
+.nope
 	and a
 	ret
 
 _DoesSpriteHaveFacings::
 ; Checks to see whether we can apply a facing to a sprite.
 ; Returns carry unless the sprite is a Pokemon or a Still Sprite.
+	cp SPRITE_FOLLOWER
+	jr z, .follower
 	cp SPRITE_POKEMON
 	jr nc, .only_down
 
@@ -265,11 +358,19 @@ _DoesSpriteHaveFacings::
 	scf
 	ret
 
+.follower
+	ld a, WALKING_SPRITE
+
 .only_down
 	and a
 	ret
 
 _GetSpritePalette::
+	ld a, c
+	push bc
+	call GetFollowingSprite
+	pop bc
+	jr c, .follower
 	ld a, c
 	call GetMonSprite
 	jr c, .is_pokemon
@@ -285,6 +386,38 @@ _GetSpritePalette::
 
 .is_pokemon
 	xor a
+	ld c, a
+	ret
+
+.follower
+	dec a
+	ld hl, MenuMonPals
+	ld b, 0
+	ld c, a
+	add hl, bc
+	ld a, BANK(MenuMonPals)
+	call GetFarByte
+	push de
+	ld d, a
+	ld a, [wFollowerPartyNum]
+	dec a
+	ld hl, wPartyMon1DVs
+	call GetPartyLocation
+	ld b, h
+	ld c, l
+	farcall CheckShininess
+	ld a, d
+	pop de
+	jr c, .shiny
+	swap a
+.shiny
+	and $f
+	ld hl, FollowingPalLookupTable
+	ld b, 0
+	ld c, a
+	add hl, bc
+	ld a, BANK(FollowingPalLookupTable)
+	call GetFarByte
 	ld c, a
 	ret
 
@@ -454,7 +587,17 @@ endr
 
 .bankswitch
 	ldh [rVBK], a
+
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wDecompressScratch)
+	ldh [rSVBK], a
+
 	call Get2bpp
+
+	pop af
+	ldh [rSVBK], a
+
 	pop af
 	ldh [rVBK], a
 	ret
